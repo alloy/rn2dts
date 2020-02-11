5290d56c74fec5561f645b31df6a77237d88d377
Eloy Durán <eloy.de.enige@gmail.com>
2020-02-11 22:34:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// const Runner = require("jscodeshift/src/Runner");
+// const paths = []
+// Runner.run(path.resolve(options.transform), paths, options);
+
+const convertFlowToTS = require("flow-to-ts")
